67c3f8087e44ecec1f6fe68f8a39bcfcc585a5a2
Talha Qamar <qamartalha@proton.me>
2026-01-24 14:54:36 +0500

    completed day 1 part 2

--- src/day1.cpp
+++ src/day1.cpp
@@ -2,12 +2,12 @@
 #include <fstream>
 #include <string>
 
-int main() {
+void part1(){
     std::ifstream inputFile("input/day1.txt");
     std::string line;
 
     int number_of_zeroes = 0;
-    long long int val = 50;
+    int val = 50;
 
     if (inputFile.is_open()) {
         while (std::getline(inputFile, line)) {
@@ -24,19 +24,65 @@ int main() {
             if(val > 99) {
                 val = val % 100;
             }
-            std::cout << line << " " << val << std::endl;
-
             if(val == 0){
                 number_of_zeroes++;
             }
         }
 
-        std::cout << number_of_zeroes << std::endl;
+        std::cout << "part 1:" << number_of_zeroes << std::endl;
     } else {
         // Print an error message if the file cannot be opened
         std::cerr << "Error: Unable to open file." << std::endl;
     }
-
-    return 0;
 }
 
+void part2(){
+    std::ifstream inputFile("input/day1.txt");
+    std::string line;
+
+    int number_of_zeroes = 0;
+    int val = 50;
+
+    if (inputFile.is_open()) {
+        while (std::getline(inputFile, line)) {
+            int offset = std::stoi(line.substr(1));
+            if(line[0] == 'R'){
+                for(int i = 0; i < offset; i++){
+                    val++;
+                    if(val > 99){
+                        val = 0;
+                    } else if (val < 0){
+                        val = 99;
+                    }
+
+                    if(val == 0){
+                        number_of_zeroes++;
+                    }
+                }
+            } else if(line[0] == 'L'){
+                for(int i = 0; i < offset; i++){
+                    val--;
+                    if(val > 99){
+                        val = 0;
+                    } else if (val < 0){
+                        val = 99;
+                    }
+
+                    if(val == 0){
+                        number_of_zeroes++;
+                    }
+                }
+            }
+        }
+
+        std::cout << "part 2:" << number_of_zeroes << std::endl;
+    } else {
+        // Print an error message if the file cannot be opened
+        std::cerr << "Error: Unable to open file." << std::endl;
+    }
+}
+int main() {
+    part1();
+    part2();
+    return 0;
+}
